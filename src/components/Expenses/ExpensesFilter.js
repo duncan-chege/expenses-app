8b64314ecd4db5a037d583b8504ced1a053faaf3
viisualworks <dshege4@gmail.com>
2024-01-18 17:17:02 +0300

    ignore the node modules folder

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,24 @@
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+  const years = [2022, 2021, 2020, 2019];
+
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  }
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+          {years.map((year) => (
+            <option key={year} value={year}>{year}</option>
+          ))}
+          </select>
+      </div>
+    </div>
+  );
+}
+
+export default ExpensesFilter;
